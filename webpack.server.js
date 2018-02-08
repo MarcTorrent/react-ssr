8b0d70956ea9dc55c90c0505e874b1a563817567
Marc Torrent <marc.torrent@privalia.com>
2018-02-08 16:18:14 +0100

    Initial Commit

---
+++ webpack.server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
+const webpackNodeExternals = require('webpack-node-externals');
+
+const config = {
+    // Inform webpack that we are building a bundle for nodeJS, rather than the browser
+    target: 'node',
+
+    // Tell webpack the entrypoint
+    entry: './src/index.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+
+    externals: [webpackNodeExternals()]
+};
+
+module.exports = merge(baseConfig, config);
